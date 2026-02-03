8fbb7abbc3e00190158b875444c9f59d1d544a23
Bryan Rios <bryanriosb01@gmail.com>
2026-02-03 11:45:16 -0500

    fix

--- components/StockStatusSectionCards.tsx
+++
@@ -1,9 +0,0 @@
-import StockStatusServiceLevel from './StockStatusServiceLevel'
-
-export default function StockStatusSectionCards() {
-  return (
-    <div className="flex gap-4 w-full">
-      <StockStatusServiceLevel />
-    </div>
-  )
-}
